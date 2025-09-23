bd3356e544a87cbec4df2cfafbd295b176983320
Sam-Lewi <garciasamuiyl@gmail.com>
2025-09-23 16:06:28 +0800

    change url

--- variables.tf
+++ variables.tf
@@ -17,6 +17,6 @@ variable "s3_payload_key" {
 variable "jenkins_url" {
   description = "URL of the Jenkins server"
   type = string
-  default = "https://d20e58836f14.ngrok-free.app"
+  default = " https://8d891e4a7f96.ngrok-free.app"
 }
 
